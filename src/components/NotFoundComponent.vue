ced6d63c2781f831096e1b1adb03bc74ed17be64
Sergey Talanov <stalanov@griddynamics.com>
2022-04-07 22:59:31 +0400

    extract NotFoundComponent into separate

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-card class="not-found" :title="props.title" size="huge">
+    <template #cover>
+      <img class="not-found__image" src="@/assets/404.png" />
+    </template>
+    <slot />
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { NCard } from 'naive-ui';
+
+interface NotFoundProps {
+  title: string;
+}
+const props = defineProps<NotFoundProps>();
+</script>
+
+<style scoped lang="scss">
+.not-found {
+  max-width: 600px;
+  margin: 2em auto;
+
+  &__image {
+    max-width: 300px;
+    margin: auto;
+  }
+}
+</style>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,26 +1,10 @@
 <template>
-  <n-card class="not-found" title="Not Found" size="huge">
-    <template #cover>
-      <img class="not-found__image" src="@/assets/404.png" />
-    </template>
+  <NotFoundComponent title="Not Found">
     Oops, we couldn't find that page. Try going <router-link :to="{ name: RouteName.HOME }">home</router-link>
-  </n-card>
+  </NotFoundComponent>
 </template>
 
 <script setup lang="ts">
-import { NCard } from 'naive-ui';
-
+import NotFoundComponent from '@/components/NotFoundComponent.vue';
 import { RouteName } from '@/router/types';
 </script>
-
-<style scoped lang="scss">
-.not-found {
-  max-width: 600px;
-  margin: 2em auto;
-
-  &__image {
-    max-width: 300px;
-    margin: auto;
-  }
-}
-</style>
